d561cd28cc09690ab88950de63beceef97817ca0
Conor Clarke <conorc470@gmail.com>
2017-10-04 12:26:51 +0000

    new-images

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-function myFunction() {
-            alert("Thank you. Your message has been sent!");
-}
-
-$(document).ready(function () {    
-            $('#demoModal').modal('show');
-});
+(function() {
+  "use strict";
+  window.addEventListener("load", function() {
+    var form = document.getElementById("needs-validation");
+    form.addEventListener("submit", function(event) {
+      if (form.checkValidity() == false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add("was-validated");
+    }, false);
+  }, false);
+}());
