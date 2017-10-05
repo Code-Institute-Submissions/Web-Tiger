4e7e3d936f84175e61e3df6c9ea98512c50c5ec3
Conor Clarke <conorc470@gmail.com>
2017-10-05 15:16:27 +0000

    jquery

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-$(document).ready(function(){
-    $("input").focus(function(){
-        $(this).css("background-color", "#cccccc");
-    });
-    $("input").blur(function(){
-        $(this).css("background-color", "#ffffff");
-    });
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
 });
+
+$(document).ready(function () {
+
+          $('#contactform').captcha();
+
+      });
