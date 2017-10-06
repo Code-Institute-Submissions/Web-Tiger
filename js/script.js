c4d8f20a5dcefc76544e5324605b35826b7b5272
Conor Clarke <conorc470@gmail.com>
2017-10-06 13:23:56 +0000

    new-layouts

--- js/script.js
+++ js/script.js
@@ -2,8 +2,17 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
 
-$(document).ready(function () {
+window.onscroll = function() {scrollFunction()};
 
-          $('#contactform').captcha();
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+};
 
-      });
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
